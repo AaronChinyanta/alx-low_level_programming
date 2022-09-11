fedec881fb10c4ff1b05c8849b07061178d052a1
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-11 12:57:47 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - entry point
  *
- * Description: using the main function
- * this prints
- * Programming is positive, zero, or negative
- * Return: 0
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		printf("%i", i);
-}
-printf("\n");
-return (0);
+		print("%d", i);
+	printf("\n");
+
+	return (0);
 }
