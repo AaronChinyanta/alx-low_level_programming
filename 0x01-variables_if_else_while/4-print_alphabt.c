1434f677cf565ad16efdf2689fab235b7c6466d1
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-11 12:28:08 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	char ch;
 	for (ch = 'a' ; ch <= 'z'; ch++)
 
 	{
-		if (ch ! = 'e' && ch ! = 'q')
+		if (ch != 'e' && ch != 'q')
 			putchar(ch);
 	}
 	putchar('\n');
