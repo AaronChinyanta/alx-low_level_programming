83ddcb26113f4c12ddedda2d803cfbf6027ca2d1
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-12 04:52:58 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c= 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n.);
+	return(0);
+}
