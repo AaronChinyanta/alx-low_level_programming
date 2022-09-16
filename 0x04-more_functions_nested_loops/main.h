598ff7934cac239653b9b1271b335df375ddcd7f
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-16 09:00:40 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**
  * main - header filr for prototype
  * void prototypes (void)
- * int prototypes 
- */
-
+ * int prototypes (int)
+ */ 
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
