d86d761c3f7f6166604a8c549350b132a8bb6222
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-16 08:50:26 -0700

    Task o

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,21 +4,20 @@
 /**
  * main - header filr for prototype
  * void prototypes (void)
- * int prototypes (int)
+ * int prototypes 
  */
 
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
-void
-print_most_numbers(void);
+void print_most_numbers(void)
 void more_numbers(void);
-void print_line(int n);
+void print_line(int n)
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
 
-#endif
+#endif /* MAIN_H */
