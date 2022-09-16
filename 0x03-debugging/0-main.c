af842aa86debab8145097e64ac086976db308204
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-16 02:09:56 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - tests function that prints if interger is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_ornegative(i);
+
+	return (0);
+}
