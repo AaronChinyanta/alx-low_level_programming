5aa1eacb4ac882c19cc92692fbce9feb44b64c92
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-16 09:06:14 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,9 @@
  * Return: 1 whether it is, 0 otherwise
  */
 int _isupper(int c)
-
 {
 	if ((c >= 'A') && (c <= 'Z'))
-
-	{
-		return (1);
-	}
-
-	return (0);
+		return (i);
+	else
+		return (0);
 }
