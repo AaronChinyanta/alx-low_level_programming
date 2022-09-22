d4623ffcc9f9e38624769bf1c0994af775a967be
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-22 08:37:07 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <main.h>
+#include <stdio.h>
 
 /**
  * _strlen - returns the length of a string
@@ -30,6 +30,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a') && (str[index] <= 'z'))
 			index++;
+
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
