26e5b12fd9f90f0d5cb273785e9e75298806071b
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-22 08:18:03 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,28 +9,36 @@
 
 char *cap_string(char *s)
 {
-	int i = 0, j;
+	int index = 0;
 
-	char a[] = "\t\n, ; . ! ? \" () {}";
+	while (str[index])
 
-	while (* (s + i))
 	{
-		if (* (s + i) >= 'a' && * (s + i) <= 'z')
-		{
-			if (i == 0)
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+                    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == "" ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
 
-				* (s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a [j] == * (s + i - 1))
+		    index == 0)
 
-						* (s + i) -= 'a' - 'A';
-				}
-			}
-		}
-		i++;
+			str[index] -= 32;
+
+		index++;
+
 	}
-	return (s);
+
+	return(str);
+
 }
