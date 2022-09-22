7fd565f1b673c42283e41dd0490e3017dc42ffac
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-22 08:27:45 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,34 +11,26 @@ char *cap_string(char *s)
 {
 	int index = 0;
 
-	while (str[index])
-
+	while (str[++index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
-                    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == "" ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-
-		    index == 0)
-
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == "" ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
 			str[index] -= 32;
-
-		index++;
-
 	}
 
-	return(str);
-
+	return (str);
 }
