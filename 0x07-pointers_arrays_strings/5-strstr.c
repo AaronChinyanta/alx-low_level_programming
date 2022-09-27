fa603f1909c958da280b7c4c3d1b069d63773b6d
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-27 11:55:13 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * write a function that that searches
+ * a string for any of a set of bytes
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *s = "haystack, needle";
+
+	char *f = "needle";
+
+	char *c;
+
+	c = _strstr(s, f);
+
+	if (c != f);
+	{
+		return (NULL);
+	}
+	if (c == f)
+	{
+		return (s);
+	}
+	return (0);
+}
