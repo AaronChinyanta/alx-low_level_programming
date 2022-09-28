ad0014c54ff7d48e7fb12ec880603d1e02d81eac
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-09-28 04:55:13 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,11 @@
  * @s: string
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
 
 void _puts_recursion(char *s)
+
 {
 	if (*s == 0)
 
@@ -16,6 +17,7 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
