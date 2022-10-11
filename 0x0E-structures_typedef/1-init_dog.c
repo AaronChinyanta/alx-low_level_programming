d8c06455cfedd5a95a77c68a679e6f4f226efa25
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-10-11 20:16:27 +0200

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,9 +9,11 @@
  *
  * Return: no return.
  */
-void init_dog(struct dog *d, char *name, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
+	
 {
 	if (d)
+
 	{
 		d->name = name;
 		d->age = age;
