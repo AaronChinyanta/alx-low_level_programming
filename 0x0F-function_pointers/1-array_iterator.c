ba47d9e3813cc587724689de7973aafce6445fdb
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-10-14 01:41:53 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,12 @@
  * Return: no return.
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
 	unsigned int i;
 
-	if (array && action)
+	if(array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
