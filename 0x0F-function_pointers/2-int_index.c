2cb337c74664f08e54a00a6e75e09a7e7c2fa924
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-10-14 04:02:37 -0700

    Task 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @cmp: pointer to the function to be used
  * to compare values.
  *
- * Return: index of the first eementelement for which the cmp
+ * Return: index of the first element for which the cmp
  * function does not return 0. If no elements matches,
  * return -1. If seize <= 0, return -1.
  */
@@ -21,6 +21,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (-1);
 
 		for (i = 0; i < size; i++)
+
 			if (cmp(array[i]))
 				return (i);
 	}
