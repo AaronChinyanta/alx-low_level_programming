d30bc722997e1d4202aefe32ae83427296a3b199
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-10-21 04:13:37 -0700

    Task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,6 @@
  *
  * Return: The number of nodes in h.
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
@@ -15,10 +14,12 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+
 			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		nodes++;
 		h = h->next;
 	}
