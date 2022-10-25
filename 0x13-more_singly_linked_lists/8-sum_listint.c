5f4193d86d53587b22f199989d6825f20e6a05ff
AaronChinyanta <aaronyohchiny23@gmail.com>
2022-10-25 10:45:52 -0700

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 
 /**
- * sum_listint
+ * sum
  * a linked list.
- * @head: sum of all the data (n).
+ * @head: sum of all the data 
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
 
 	sum = 0;
-
 	while (head != NULL)
 	{
 		sum += head->n;
